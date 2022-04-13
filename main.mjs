6d93d575ccea6836bfcb027b14677742e167a1c1
leonardorledesma <leo.ledesma@gmail.com>
2022-04-13 07:40:38 -0300

    primeiro commit

---
+++ main.mjs
@@ -0,0 +1,5 @@
+import{mostraIdade, mostraCidade, mostraHobby} from './funcoes.mjs';
+
+console.log(mostraIdade('Leonardo', '26'));
+console.log(mostraCidade('Leonardo', 'São Paulo'));
+console.log(mostraHobby('Leonardo', 'Jogar video game'));
